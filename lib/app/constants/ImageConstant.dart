0831f98ab3019b6985fee737b18554baf75fbd1e
jigar.mangukiya <jigz.mangukiya4898@gmail.com>
2020-06-09 23:18:53 +0530

    changes

--- lib/app/constants/ImageConstant.dart
+++ lib/app/constants/ImageConstant.dart
@@ -1,5 +1,3 @@
 //Menu
-const String home = "assets/BottomDrawer/HomeIcon.png";
-const String settings = "assets/BottomDrawer/settings.png";
-const String transfer = "assets/BottomDrawer/transfer.png";
-const String user = "assets/BottomDrawer/user.png";
+
+const String imageBanner = "assets/bannerApp.webp";
